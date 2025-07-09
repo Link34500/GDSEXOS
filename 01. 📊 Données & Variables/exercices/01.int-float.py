313611e954b17ba896bd7825b8d774030ae3e1ba
Link34500 <fredi.guiose@gmail.com>
2025-07-09 13:46:11 -0300

    UPDATE

---
+++ 01. 📊 Données & Variables/exercices/01.int-float.py
@@ -0,0 +1,57 @@
+# ---- NIVEAU 1 ----
+
+# Afficher le nombre 5
+
+
+# Afficher la somme de 5 et 7 (addition)
+
+
+# Afficher la différence entre 6 et 3 (soustraction)
+
+
+# Afficher le produit de 5 et 4 (multiplication)
+
+
+# Afficher la puissance de 4 exposant 6
+
+
+# Afficher le quotient de 8 divisé par 2 (division décimale)
+
+
+# Afficher le quotient de 9 divisé par 4 (division décimale)
+
+
+# Afficher le quotient entier de 8 divisé par 2 (division euclidienne)
+
+
+# Afficher le quotient entier de 9 divisé par 4 (division euclidienne)
+
+
+# Afficher le reste de la division de 8 par 2
+
+
+# Afficher le reste de la division de 9 par 4
+
+
+# ---- NIVEAU 2 ----
+
+# Afficher la somme de 5 et de la différence de 10 et 4 (N + soustraction)
+
+
+# Afficher le produit de 3 et de la somme de 2 et 7 (N * addition)
+
+
+# Afficher la différence entre 20 et le produit de 4 et 3 (N - multiplication)
+
+
+# Afficher la puissance de la somme de 2 et 3 exposant 4 (addition ** N)
+
+
+# Afficher le quotient entier de la somme de 15 et 5 divisé par 4 (addition // N)
+
+
+# Afficher le reste de la division de la différence de 25 et 10 par 6 (soustraction % N)
+
+
+# Afficher le quotient décimal de la somme de 7 et 5 divisé par la différence de 12 et 8 (addition / soustraction)
+
